9f3988705d4806c2f1f8d106fe4488b05cb6530f
novakvova <novakvova@gmail.com>
2019-03-06 09:55:32 +0200

    Begin work login page

---
+++ WebSiteCore/ClientApp/src/components/auth/login/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <Row>
+                <Col md={4} mdOffset={4}>
+                    <h1>Login page</h1>
+                </Col>
+            </Row>
+        );
+    }
+}
+export default LoginPage;

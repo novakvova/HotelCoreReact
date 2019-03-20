5742d597338bf809f1eb804fb9b2f1a8aca36bf1
NickHoch <kolya.goch@gmail.com>
2019-03-20 19:30:21 +0200

    some changes

---
+++ WebSiteCore/Controllers/ApartmentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebSiteCore.DAL.Entities;
+
+namespace WebSiteCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentController : ControllerBase
+    {
+        private readonly EFDbContext _ctx;
+        public ApartmentController(EFDbContext _ctx)
+        {
+            this._ctx = _ctx;
+        }
+        public IActionResult Get()
+        {
+            return Ok(_ctx.Apartments.ToList());
+        }
+    }
+}

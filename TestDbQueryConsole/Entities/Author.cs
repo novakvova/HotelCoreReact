d8ac39521486f9831c571feca20197425a62c503
Hoch <kolya.goch@gmail.com>
2019-03-21 12:12:06 +0200

    console project was added

---
+++ TestDbQueryConsole/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TestDbQueryConsole.Entities
+{
+    [Table("tblAuthors")]
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ TestDbQueryConsole/Entities/AuthorArticleCount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TestDbQueryConsole.Entities
+{
+    [Table("vAuthorArticleCount")]
+    public class AuthorArticleCount
+    {
+        public string Name { get; private set; }
+        public int ArticleCount { get; private set; }
+    }
+
+    /*
+    
+    
+CREATE VIEW [dbo].[vAuthorArticleCount]
+AS 
+SELECT  a.Name,
+  Count(r.Id) as ArticleCount
+from tblAuthors a
+  JOIN tblArticles r on r.Id = a.Id
+GROUP BY a.Name
+     */
+}

f0a00bde558853df083b5807b57745db8aaa32b7
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-03-25 00:06:11 +0200

    Update Offers

--- WebSiteCore/ClientApp/src/components/offers/Offers.js
+++ WebSiteCore/ClientApp/src/components/offers/Offers.js
@@ -9,24 +9,29 @@ function Offer(props) {
             <div className='container offer-card'>
                 <div className='box'>
                     <Link to='/'>
-                        <img src={props.img} />
+                        <img src={props.img} className='img' />
                     </Link>
                 </div>
                 <div className='offer-form'>
-                    <div className='box title'>
-                        <p>
+                    <div className='box'>
+                        <h3 className='title'>
                             {props.title}
-                        </p>
+                        </h3>
                     </div>
-                    <div className='box date'>
-                        <p>
+                    <div className='right'>
+                        <h3 className='date'>
                             Valid: {props.date}
-                        </p>
+                        </h3>
                     </div>
-                    <div className=' description'>
-                        <p>
+                    <div>
+                        <p className='description'>
                             {props.description}
                         </p>
+                    </div>
+                    <div>
+                        <h4 className='bottom-title'>
+                             Special offers and discounts are not summed up.
+                        </h4>
                     </div>
                 </div>
             </div>
@@ -48,12 +53,14 @@ class Offers extends Component {
                 <Offer img='http://cityhotel.ua/wp-content/uploads/2019/02/spring-e1551256058479.jpeg'
                     title='Spring Weekdays' date='04.03.2019 - 26.04.2019'
                     description='Great offer in CITYHOTEL! 15% OFF for accommodation on weekdays!' />
-                <Offer img='http://cityhotel.ua/wp-content/uploads/2019/02/spring-e1551256058479.jpeg'
-                    title='Spring Weekdays' date='04.03.2019 - 26.04.2019'
-                    description='Great offer in CITYHOTEL! 15% OFF for accommodation on weekdays!' />
-                <Offer img='http://cityhotel.ua/wp-content/uploads/2019/02/spring-e1551256058479.jpeg'
-                    title='Spring Weekdays' date='04.03.2019 - 26.04.2019'
-                    description='Great offer in CITYHOTEL! 15% OFF for accommodation on weekdays!' />
+                <Offer img='http://cityhotel.ua/wp-content/uploads/2018/10/frisatsun-e1540809838517.jpeg'
+                    title='FriSatSun' date='04.03.2019 - 26.04.2019'
+                    description='Your discount on weekends (Fri - Sun):
+                    •	1 day 15% OFF 
+                    •	2-3 days 25% OFF ' />
+                <Offer img='http://cityhotel.ua/wp-content/uploads/2018/09/Untitled-design-32-e1551462786800.jpeg'
+                    title='Weekend Rate' date='03.03.2019 - 26.04.2019'
+                    description='25% OFF for accommodation on weekend (FRI till MON)' />
             </div>
         )
     };

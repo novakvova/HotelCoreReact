f7d20c76e5ea26450a611a36ebb5398f9358b4e0
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-03-27 22:21:02 +0200

    update navbar booking-btn

--- WebSiteCore/ClientApp/src/components/navmenu/NavMenu.js
+++ WebSiteCore/ClientApp/src/components/navmenu/NavMenu.js
@@ -151,7 +151,7 @@ class NavMenu extends Component {
                 <NumericInput className='numeric'/>              
             </div>
             <div className='box'>
-              <input className='button' type='button' value='BOOK NOW' />
+              <Link to='/booking' className='booking-btn'>BOOKING NOW</Link>
             </div>
           </div>
         </Row>

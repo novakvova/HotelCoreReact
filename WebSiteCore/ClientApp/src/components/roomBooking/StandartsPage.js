25c57bdae69b60c789b1bbf84745ec8552cfaf8c
MELNYK <dmelnyk231098@gmail.com>
2019-03-28 09:59:10 +0200

    rooms page complited

--- WebSiteCore/ClientApp/src/components/roomBooking/StandartsPage.js
+++ WebSiteCore/ClientApp/src/components/roomBooking/StandartsPage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import RoomStandart from './RoomStandart'
+import { Row } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <RoomStandart />
-       <RoomStandart />
-       <RoomStandart />
-       <RoomStandart />
-       <RoomStandart />
-       <RoomStandart />
+        <Row className='headerRow' style={{textAlign: 'center' , width: '100%'}}>
+          <hr className='leftHr' width="20%" />
+          <h1 className='header'>HOTEL ROOMS</h1>
+          <hr className='rightHr' width="20%" />
+          <hr className='rightHr' width="80%"/>
+        </Row>
+        <RoomStandart />
+        <RoomStandart />
+        <RoomStandart />
+        <RoomStandart />
+        <RoomStandart />
+        <RoomStandart />
       </div>
     );
   }
